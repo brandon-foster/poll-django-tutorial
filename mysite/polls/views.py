05340e48d5a5cb8028b1cebfdcadf6bd2bf535d8
Brandon-Foster <brandon.foster.vt@gmail.com>
2013-12-22 23:12:46 -0500

    fix typo

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -8,7 +8,7 @@ def detail(request, question_id):
     return HttpResponse("You're looking at question %s." % question_id)
 
 def results(request, question_id):
-    return HttpResponse(response % question_id)
+    return HttpResponse("You're looking at results from %s" % question_id)
 
 def vote(request, question_id):
     return HttpResponse("You're voting on question %s" % question_id)
