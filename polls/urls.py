2b27c16792152523d41f9c697e5a9203c8dfa1ae
Brandon-Foster <brandon.foster.vt@gmail.com>
2014-01-09 15:57:49 -0500

    modify the keyward aguments in the URLconf
    the DetailView generic view requires they key value captured from the URL
    to be called "pk"

--- polls/urls.py
+++ polls/urls.py
@@ -5,11 +5,18 @@ from polls import views
 urlpatterns = patterns('',
     # ex: /polls/
     url(r'^$', views.IndexView.as_view(), name='index'),
+    
     # ex: /polls/5/
     # the 'name' value as called by the {% url %} template tag
-    url(r'^(?P<poll_id>\d+)/$', views.DetailView.as_view(), name='detail'),
+    # the DetailView generic view (see views.py) requires the key value
+    # captured from the URL to be called "pk"
+    url(r'^(?P<pk>\d+)/$', views.DetailView.as_view(), name='detail'),
+    
     # ex: /polls/5/results/
-    url(r'^(?P<poll_id>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    # the DetailView generic view (see views.py) requires the key value
+    # captured from the URL to be called "pk"
+    url(r'^(?P<pk>\d+)/results/$', views.ResultsView.as_view(), name='results'),
+    
     # ex: /polls/5/vote/
     url(r'^(?P<poll_id>\d+)/vote/$', views.vote, name='vote'),
 )
