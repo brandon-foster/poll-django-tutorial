fe7a751f9b9d525770896085165dc36e9d236081
Brandon-Foster <brandon.foster.vt@gmail.com>
2014-01-10 15:38:26 -0500

    write tests for DetailView

--- polls/tests.py
+++ polls/tests.py
@@ -16,6 +16,26 @@ def create_poll(question, days):
     return Poll.objects.create(question=question,
                                pub_date=timezone.now() + datetime.timedelta(days=days))
 
+class PollIndexDetailTests(TestCase):
+    def test_detail_view_with_a_future_poll(self):
+        """
+        The detail view of a poll with a pub_date in the future should return a
+        404 not found.
+        """
+        future_poll = create_poll(question="Future poll.", days=5)
+        response = self.client.get(reverse('polls:detail', args=(future_poll.id,)))
+        self.assertEqual(response.status_code, 404)
+        
+    def test_detail_view_with_a_past_poll(self):
+        """
+        The detail view of a poll with a pub_date in the past should dispaly
+        the poll's question.
+        """
+        past_poll = create_poll(question="Pastt poll.", days=-5)
+        response = self.client.get(reverse('polls:detail', args=(past_poll.id,)))
+        self.assertContains(response, past_poll.question, status_code=200)    
+
+
 class PollViewTests(TestCase):
     def test_index_view_with_no_polls(self):
         """
@@ -42,7 +62,7 @@ class PollViewTests(TestCase):
         Polls with a pub_date in the future should not be displayed on the
         index page.
         """
-        create_poll(question="Future poll.", days="30")
+        create_poll(question="Future poll.", days=30)
         response = self.client.get(reverse('polls:index'))
         self.assertContains(response, "No polls are available.", status_code=200)
         self.assertQuerysetEqual(response.context['latest_poll_list'], [])
